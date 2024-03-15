b8b89e42e1a7c02659efac4cc58536af71b50a0c
L3n4mi8 <milenamariash@gmail.com>
2024-03-15 10:09:13 -0600

    switchv1

---
+++ src/switch.js
@@ -0,0 +1,37 @@
+document.querySelector('#header').innerHTML="estoy leyendo el documento"
+var fruta
+var mensaje
+var userInput =prompt("ingrese el sabor de su fruta")
+
+function jugo_de_fresas() {
+    document.querySelector('#header').innerHTML = mensaje;
+  }
+  
+  function jugo_de_naranja() {
+    mensaje = "batir naranjas y mandarinas"
+    document.querySelector('#header').innerHTML = mensaje;
+  }
+
+  
+  
+  function servir_vaso_de_agua() {
+    mensaje = "servir vaso con agua"
+    document.querySelector('#header').innerHTML = mensaje
+    }
+
+
+ fruta = userInput.toLowerCase()
+
+switch(fruta) {
+    case "fresa":
+        mensaje = "batir fresas"
+        jugo_de_fresas()
+     break
+     case "naranja":
+     case "mandarina":
+        jugo_de_naranja()
+        break
+        default:
+        servir_vaso_de_agua()
+        break
+}
