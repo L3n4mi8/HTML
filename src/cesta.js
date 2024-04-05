e6cd3ec5548e8f63d043e825364bad13b75b07cd
L3n4mi8 <milenamariash@gmail.com>
2024-04-05 11:11:41 -0600

    pract1

---
+++ src/cesta.js
@@ -0,0 +1,6 @@
+ocument.querySelector('#header').innerHTML = "estoy leyendo el documento cesta"
+
+var cesta
+
+var i = 0
+var respuesta = prompt("1. Ver elementos\n2. Agregar \n3. multiplicacion\n4. division\n5. Salir")
