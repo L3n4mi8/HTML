1465cca2a05a838752baeff189bb820fd282ebb1
L3n4mi8 <milenamariash@gmail.com>
2024-04-11 10:05:45 -0600

    practicacesta

--- src/cesta.js
+++ src/cesta.js
@@ -1,6 +1,58 @@
-ocument.querySelector('#header').innerHTML = "estoy leyendo el documento cesta"
+document.querySelector('#header').innerHTML = "estoy leyendo el documento cesta"
 
-var cesta
+var cesta = []
 
-var i = 0
-var respuesta = prompt("1. Ver elementos\n2. Agregar \n3. multiplicacion\n4. division\n5. Salir")
+
+
+function Ver() {
+   alert(cesta)
+}
+
+function agregar() {
+    var nueva_fruta = prompt("Ingrese nueva fruta")
+    cesta.push(nueva_fruta)
+}
+
+
+function eliminar() {
+    cesta.pop()
+    alert("Se elimino con exito")
+}
+
+function eliminar_fruta_especifica(){
+    prompt("Ingrese la fruta que desea eliminar")
+  
+}
+
+
+var inicio = true
+
+do {
+
+
+
+    var respuesta = prompt("1. Ver elementos de la cesta \n2. Agregar una fruta a la cesta \n3.eliminar una fruta de la cesta \n4.Eliminar fruta especifica \n5.Svalir")
+    switch (parseInt(respuesta)) {
+
+        case 1:
+            Ver();
+            break
+
+        case 2:
+            agregar();
+            break
+
+        case 3:
+            eliminar();
+            break
+
+        case 4:
+            
+            break 
+            
+        case 5:
+            
+            inicio = false
+    }
+
+} while (inicio == true)
